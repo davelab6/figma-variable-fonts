4f11e78bf93c044a8b7165542ac9bd4c07e2aa2b
Leonard Bogdonoff <rememberlenny@gmail.com>
2020-05-11 23:16:07 -0400

    Get the samsa code coming from a node module

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,48 @@
-  
 import * as React from 'react';
+import {SamsaFont} from 'samsa';
 
-import AboutModule from './components/AboutModule'
-import AxesModule from './components/AxesModule'
-import FontSetupModule from './components/FontSetupModule'
-import InfoModule from './components/InfoModule'
-import WebfontModule from './components/WebfontModule'
+import AboutModule from './components/AboutModule';
+import AxesModule from './components/AxesModule';
+import FontSetupModule from './components/FontSetupModule';
+import InfoModule from './components/InfoModule';
+import WebfontModule from './components/WebfontModule';
 
 const App = () => {
-  let textbox: HTMLInputElement;
+    let textbox: HTMLInputElement;
 
-  const countRef = (element: HTMLInputElement) => {
-    if (element) element.value = '5'
-    textbox = element
-  }
+    console.log('SamsaFont', SamsaFont);
 
-  const onCreate = () => {
-    const count = parseInt(textbox.value, 10)
-    parent.postMessage({ pluginMessage: { type: 'create-rectangles', count } }, '*')
-  }
+    const countRef = (element: HTMLInputElement) => {
+        if (element) element.value = '5';
+        textbox = element;
+    };
 
-  const onCancel = () => {
-    parent.postMessage({ pluginMessage: { type: 'cancel' } }, '*')
-  }
+    const onCreate = () => {
+        const count = parseInt(textbox.value, 10);
+        parent.postMessage({pluginMessage: {type: 'create-rectangles', count}}, '*');
+    };
 
-  return (
-    <>
-      <h2>Rectangle Creator</h2>
-      <p>Count: <input ref={countRef} /></p>
-      <button id="create" onClick={onCreate}>Create</button>
-      <button onClick={onCancel}>Cancel</button>
-      <InfoModule />
-      <WebfontModule />
-      <AxesModule />
-      <FontSetupModule />
-      <AboutModule />
-    </>
-  );
-}
+    const onCancel = () => {
+        parent.postMessage({pluginMessage: {type: 'cancel'}}, '*');
+    };
+
+    return (
+        <>
+            <h2>Rectangle Creator</h2>
+            <p>
+                Count: <input ref={countRef} />
+            </p>
+            <button id="create" onClick={onCreate}>
+                Create
+            </button>
+            <button onClick={onCancel}>Cancel</button>
+            <InfoModule />
+            <WebfontModule />
+            <AxesModule />
+            <FontSetupModule />
+            <AboutModule />
+        </>
+    );
+};
 
 export default App;
