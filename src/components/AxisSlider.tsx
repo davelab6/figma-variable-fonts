2d801d68bb5d1d2d5201d30805ea34fb2955a507
Leonard Bogdonoff <rememberlenny@gmail.com>
2020-05-12 03:10:32 -0400

    Correctly style the axis slider

--- src/components/AxisSlider.tsx
+++ src/components/AxisSlider.tsx
@@ -36,18 +36,57 @@ const SliderLabels = styled.div`
 const SliderLabelsLeft = styled.div`
     display: flex;
     justify-content: flex-start;
-    align-items: center;
+    align-items: baseline;
 `;
 
 const SliderLabelsRight = styled.div``;
 
-const SliderRow = styled.div``;
+const SliderRow = styled.div`
+    .rangeslider-horizontal .rangeslider__fill {
+        background: #efefef;
+    }
+
+    .rangeslider,
+    .rangeslider .rangeslider__fill {
+        box-shadow: none;
+    }
+
+    .rangeslider-horizontal {
+        height: 8px;
+        border-radius: 10px;
+    }
+
+    .rangeslider {
+        margin: 0px;
+    }
+
+    .rangeslider-horizontal .rangeslider__handle {
+        width: 8px;
+        height: 8px;
+        border-radius: 8px;
+        top: 50%;
+        transform: translate3d(-50%, -50%, 0);
+    }
+
+    .rangeslider .rangeslider__handle {
+        background: #d7d7d7;
+        box-shadow: none;
+    }
+
+    .rangeslider-horizontal .rangeslider__handle:after {
+        display: none;
+    }
+`;
+
+const AxisSliderWrapper = styled.div`
+    margin-bottom: 16px;
+`;
 
 function AxisSlider({name, tag, min, defaultValue, max}: AxisSliderProps) {
     const [current, setCurrent] = useState(defaultValue);
 
     return (
-        <>
+        <AxisSliderWrapper>
             <SliderLabels>
                 <SliderLabelsLeft>
                     <AxisTag>{tag}</AxisTag>
@@ -58,9 +97,9 @@ function AxisSlider({name, tag, min, defaultValue, max}: AxisSliderProps) {
                 </SliderLabelsRight>
             </SliderLabels>
             <SliderRow>
-                <Slider min={min} max={max} value={current} handleLabel={`${current}`} onChange={setCurrent} />
+                <Slider tooltip={false} min={min} max={max} value={current} onChange={setCurrent} />
             </SliderRow>
-        </>
+        </AxisSliderWrapper>
     );
 }
 
