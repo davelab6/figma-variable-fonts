334ebc242a7895d13e5da37483d335ddb5360072
Leonard Bogdonoff <rememberlenny@gmail.com>
2020-05-24 16:25:12 -0400

    Update tests

--- src/components/__tests__/AboutModule.spec.tsx
+++ src/components/__tests__/AboutModule.spec.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
+import {cleanup, render} from '@testing-library/react';
 import AboutModule from '../AboutModule';
-import {render} from '@testing-library/react';
 
 describe('AboutModule', () => {
+    afterEach(cleanup);
     it('renders correctly', () => {
-        const el = render(<AboutModule />);
-        expect(el).toContain('Variable fonts plugin');
+        const {container} = render(<AboutModule />);
+        expect(container).toContain('Variable fonts plugin');
     });
 });
