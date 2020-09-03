99ae4f377a4872147cc7a3e2b2a219a88c2d4c1c
Leonard Bogdonoff <rememberlenny@gmail.com>
2020-09-03 01:07:09 -0400

    Pass the content

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,7 +6,16 @@ import {route} from './routes';
 import store from '../store/configStore';
 
 class App extends React.Component<{}, {}> {
+    constructor(props: any) {
+        super(props);
+
+        window.addEventListener('message', (event) => {
+            console.log('Success', event);
+        });
+    }
     public render(): JSX.Element {
+        parent.postMessage({pluginMessage: {type: 'get-selected-text'}}, '*');
+
         console.log('store', store);
         return (
             <Provider store={store}>
--- src/plugin/controller.ts
+++ src/plugin/controller.ts
@@ -5,6 +5,22 @@ figma.showUI(__html__, {
     height: 900,
 });
 
-figma.ui.onmessage = (msg) => {
-    figma.closePlugin();
-};
+figma.on('selectionchange', () => {
+    figma.ui.postMessage({
+        type: 'selection-change',
+        payload: onSelectChange(),
+    });
+});
+
+function onSelectChange() {
+    if (figma.currentPage.selection.length !== 1) {
+        return {type: 'selected-change', status: 'error', payload: {message: 'Select a single node.'}};
+    }
+
+    const node = figma.currentPage.selection[0];
+    if (node.type !== 'TEXT') {
+        return {type: 'selected-change', status: 'error', payload: {message: 'Select a single text node.'}};
+    }
+
+    return {type: 'selected-change', status: 'success', payload: {content: node.characters, fontSize: node.fontSize}};
+}
