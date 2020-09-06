8129cfdc3a1c249bd0f0e0c600f7030f500e05f8
Leonard Bogdonoff <rememberlenny@gmail.com>
2020-09-06 12:13:46 -0400

    Correct the layout in files

--- src/features/activeText/activeTextSlice.ts
+++ src/features/activeText/activeTextSlice.ts
@@ -1,38 +1,39 @@
 import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+import {STATUSES} from '../../plugin/shared/constants';
 
 interface SelectedText {
-    status: string;
-    message: string;
-    content: string;
-    fontSize: number;
+  status: string;
+  message: string;
+  content: string;
+  fontSize: number;
 }
 
 type CurrentActiveTextState = {} & SelectedText;
 
 let initialState: CurrentActiveTextState = {
-    status: 'not-loaded',
-    message: '',
-    content: 'This is a test',
-    fontSize: 14,
+  status: 'not-loaded',
+  message: '',
+  content: 'This is a test',
+  fontSize: 14,
 };
 
 const activeTextSlice = createSlice({
-    name: 'activeText',
-    initialState,
-    reducers: {
-        updateSelection(state, action: PayloadAction<SelectedText>) {
-            const {status, message, content, fontSize} = action.payload;
-            state.status = status;
-            if (status === 'error') {
-                state.message = message;
-            }
-            if (status === 'success') {
-                state.message = '';
-                state.content = content;
-                state.fontSize = fontSize;
-            }
-        },
+  name: 'activeText',
+  initialState,
+  reducers: {
+    updateSelection(state, action: PayloadAction<SelectedText>) {
+      const {status, message, content, fontSize} = action.payload;
+      state.status = status;
+      if (status === STATUSES.ERROR) {
+        state.message = message;
+      }
+      if (status === STATUSES.SUCCESS) {
+        state.message = '';
+        state.content = content;
+        state.fontSize = fontSize;
+      }
     },
+  },
 });
 
 export const {updateSelection} = activeTextSlice.actions;
