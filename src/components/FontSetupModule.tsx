894cfa9bee328533d07518e21edaa14d442d0431
Leonard Bogdonoff <rememberlenny@gmail.com>
2020-09-06 15:07:09 -0400

    Add reference to recursive

--- src/components/FontSetupModule.tsx
+++ src/components/FontSetupModule.tsx
@@ -6,62 +6,65 @@ import {useDispatch} from 'react-redux';
 import ModuleWrapper from '../layouts/ModuleWrapper';
 import {addFontFamily, updateActiveFont, updateFontAxis} from '../features/fontData/fontDataSlice';
 
+const interFont = 'https://cdn.jsdelivr.net/gh/google/fonts@master/ofl/inter/Inter[slnt,wght].ttf';
+const recursiveFont = 'https://variable-font.s3.amazonaws.com/Recursive_VF_1.063.ttf';
+
 function FontSetupModule() {
-    const dispatch = useDispatch();
-    useEffect(() => {
-        const fontUrl = 'https://cdn.jsdelivr.net/gh/google/fonts@master/ofl/inter/Inter[slnt,wght].ttf';
-        const samsaOptions = {
-            url: fontUrl,
-            fontFamily: 'DefaultFont',
-            callback: (data: {[key: string]: any}) => {
-                console.log('data', data);
-                const fontData = {
-                    fontUrl,
-                    instances: data.instances,
-                    axes: data.axes,
-                    filename: data.filename,
-                    names: data.names,
-                    fontName: data.names[6],
-                };
-                dispatch(addFontFamily(fontData));
-                const axes = {};
-                data.axes.forEach((axis) => {
-                    axes[axis.tag] = axis.default;
-                });
-                const activeFont = {
-                    fontName: data.names[6],
-                    variantName: 'Custom',
-                    axes,
-                };
-                console.log('activeFont', activeFont);
-                dispatch(updateActiveFont(activeFont));
-
-                // const axisDefaults = {};
-                // data.axes.forEach((axis) => {
-                //     axisDefaults[axis.tag] = axis.default;
-                // });
-                // dispatch(updateFontAxis(axisDefaults));
-            },
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const fontUrl = recursiveFont;
+    const samsaOptions = {
+      url: fontUrl,
+      fontFamily: 'DefaultFont',
+      callback: (data: {[key: string]: any}) => {
+        console.log('data', data);
+        const fontData = {
+          fontUrl,
+          instances: data.instances,
+          axes: data.axes,
+          filename: data.filename,
+          names: data.names,
+          fontName: data.names[6],
         };
-        const vf = new SamsaFont(samsaOptions);
-        console.log('vf', vf);
-    }, []);
+        dispatch(addFontFamily(fontData));
+        const axes = {};
+        data.axes.forEach((axis) => {
+          axes[axis.tag] = axis.default;
+        });
+        const activeFont = {
+          fontName: data.names[6],
+          variantName: 'Custom',
+          axes,
+        };
+        console.log('activeFont', activeFont);
+        dispatch(updateActiveFont(activeFont));
 
-    return (
-        <ModuleWrapper title="Font setup" open={true}>
-            <div>
-                <select id="select-fonts">
-                    <option value="volvo">Roboto</option>
-                    <option value="saab">Decovar</option>
-                    <option value="vw">Amstelvar</option>
-                </select>
-            </div>
-            <div>
-                <button value="">Upload a variable font here</button>
-            </div>
-            <div>Preview</div>
-        </ModuleWrapper>
-    );
+        // const axisDefaults = {};
+        // data.axes.forEach((axis) => {
+        //     axisDefaults[axis.tag] = axis.default;
+        // });
+        // dispatch(updateFontAxis(axisDefaults));
+      },
+    };
+    const vf = new SamsaFont(samsaOptions);
+    console.log('vf', vf);
+  }, []);
+
+  return (
+    <ModuleWrapper title="Font setup" open={true}>
+      <div>
+        <select id="select-fonts">
+          <option value="volvo">Roboto</option>
+          <option value="saab">Decovar</option>
+          <option value="vw">Amstelvar</option>
+        </select>
+      </div>
+      <div>
+        <button value="">Upload a variable font here</button>
+      </div>
+      <div>Preview</div>
+    </ModuleWrapper>
+  );
 }
 
 export default FontSetupModule;
